506de09de772b2beeca257dbf72a28a083063f77
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2024-03-27 14:32:14 +0100

    Getting started with PHP

--- 1_intro/index.php
+++ 1_intro/index.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "Hello World!";
+echo "Hello World! <br>" ;
 
+
+// phpinfo();
+
+echo "Teste <br>";
+EchO "teste <br>";
+
+$nome = "michelle ";
+echo $nome;
+echo "<br>";
+echo $NOME //case sensitive
 ?>
