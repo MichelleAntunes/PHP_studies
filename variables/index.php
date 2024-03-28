bff57e7a906adb27ed90e54b46969ff90015b8a2
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2024-03-28 13:45:33 +0100

    Simple exercises for calculating complete expressions and operators.

--- variables/index.php
+++ variables/index.php
@@ -82,3 +82,69 @@ echo 20 % 12; // gera o resto da divisão
 echo " módulo <br>";
 echo 20/12;
 echo " divisão <br>";
+
+echo 5**2;
+echo "<br>";
+
+?>
+
+<?php
+// auto incremento 
+$test = 0;
+$test2 = 0;
+
+$test++;
+$test2 = $test2 +1;
+
+echo "x: " . $test . "<br>";
+echo "y: " . $test2 . "<br>";
+
+//auto decremento
+
+$dec = 10;
+$dec2 = 10;
+
+$dec--;
+$dec2 = $dec2 - 1;
+
+echo "x: " . $dec . "<br>";
+echo "y: " . $dec2 . "<br>";
+
+//operadores de comparação:(retorna boolean)
+// == igualdade
+// === idêntico
+// != diferença
+// !== não idêntico
+// maior e maior ou igual > e >=
+// menor e menor ou igual < e <=
+// && AND somente quando as duas são verdadeiras
+// || OR
+// ! NOT
+
+//OPERADOR DE CAST - CONVERSÃO
+$a = (int) "12"; //força a virar número inteiro
+
+echo $a . "<br>";
+echo $a + 10 . "<br>";  
+
+// OEPRADOR DE ATRIBUIÇÃO
+
+$d = 0;
+$d += 10; // &d = $d + 10
+echo $d . "<br>";
+
+$e = 0;
+$e -= 10; // &e = $e - 10
+echo $e . "<br>";
+
+$c = 0;
+$c *= 10; // &c = $c * 10
+echo $c . "<br>";
+
+$f = 100;
+$f /= 10; // &f = $f / 10
+echo $f . "<br>";
+
+//OPERADOR TERNÁRIO
+
+echo 5 > 2 ?  "5 é maior que 2" :  "5 é menor que 2";
